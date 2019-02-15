d22aaa96a01653a49e8cd362aaade3ac886bc8fb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-15 07:28:00 +0000

    Deploy zeroNav/zeronav.github.io to github.com/zeroNav/zeronav.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -21,10 +21,10 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "f6af7387f5abe72e30f6a0f849c6d12e"
+    "revision": "45ccd2ded66a912982b962836ec30f12"
   },
   {
-    "url": "assets/css/0.styles.fb166ef0.css",
+    "url": "assets/css/0.styles.69809fb0.css",
     "revision": "14a656f0cb878c5ab67c1abed8d59f80"
   },
   {
@@ -96,8 +96,8 @@ self.__precacheManifest = [
     "revision": "bc54c3d3e0e14addd01a6e8cf03e4e43"
   },
   {
-    "url": "assets/js/vendor.commons.fb166ef0.js",
-    "revision": "e089fc25de697d556b8650e510d41fe8"
+    "url": "assets/js/vendor.commons.69809fb0.js",
+    "revision": "68266773d5a3dd46f05ef010bc4e8888"
   },
   {
     "url": "assets/js/vendor.vue.63bbd286.js",
@@ -105,67 +105,67 @@ self.__precacheManifest = [
   },
   {
     "url": "blogs/accounts.html",
-    "revision": "11ff94449dd4c62c5c4101d5cd96d2ae"
+    "revision": "62bb0469fce1949c3c58e4e782458681"
   },
   {
     "url": "blogs/BeyondCompare.html",
-    "revision": "2ec99763eb3979a1950591ee4b516505"
+    "revision": "f5ada90f613cf332a3c8db78837be279"
   },
   {
     "url": "blogs/charles.html",
-    "revision": "675d45b08595f1604fe6960c5fa302ad"
+    "revision": "7f914ebd185e44ba937fd28831ed2f20"
   },
   {
     "url": "blogs/earn.html",
-    "revision": "89ce22ce0b1680058f5a8f3f1839e445"
+    "revision": "fe7464fd9a6db0925dc8d718400fabd3"
   },
   {
     "url": "blogs/index.html",
-    "revision": "1e6f915572e8ee230cfed01551fae052"
+    "revision": "e41ac8b157bfd130031238e342a56f1e"
   },
   {
     "url": "blogs/mac-app.html",
-    "revision": "f375c5a0c7f4d9c2056003bf279c6041"
+    "revision": "81e3f89829013bc99c847c9d5836ed97"
   },
   {
     "url": "blogs/vagrant.html",
-    "revision": "5cf67ff93eb3062865955ffab8e9a6b4"
+    "revision": "349ab7664abca5abbaf1782bdaf1a40a"
   },
   {
     "url": "blogs/vps.html",
-    "revision": "aa4768d49e0e580252f727b25f081db8"
+    "revision": "56b81fb015e59de744f44f1fbaeea440"
   },
   {
     "url": "develop/doc.html",
-    "revision": "1436e6eca8a56e79239842b409a83759"
+    "revision": "f26705ace02302926b81e1d7810e3a19"
   },
   {
     "url": "develop/frame.html",
-    "revision": "72d48d01a869dd8cc154850214ac2456"
+    "revision": "af186db3eb14233b2c5b8d65ac14a34b"
   },
   {
     "url": "develop/ide.html",
-    "revision": "e5fae2ef0fb27a6eb6585999d014a061"
+    "revision": "85d3e86edf1024d8a13d5a59918c6d26"
   },
   {
     "url": "develop/index.html",
-    "revision": "a3592786087c0aa46f2dcbb09b52e1c3"
+    "revision": "4ee93293270646249403e31bc5fb7606"
   },
   {
     "url": "develop/map.html",
-    "revision": "a524d1981a159db9123f3493de6fc04e"
+    "revision": "f1f7dd161dc5151ab8b140b3ba1d3b44"
   },
   {
     "url": "develop/others.html",
-    "revision": "a380f3f329eada24a7888796bd37a0ef"
+    "revision": "a0b84c7e81a2ac4e56a95ffc542779d0"
   },
   {
     "url": "develop/weapp.html",
-    "revision": "30cdbfa4684a2afbc9eedd5bab519c0e"
+    "revision": "edaad3dbba440795e96cccb078d9d4ce"
   },
   {
     "url": "index.html",
-    "revision": "12a5716f5780f722177550333685ec10"
+    "revision": "174ec485290a04e6dc8ffb47731f493a"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
